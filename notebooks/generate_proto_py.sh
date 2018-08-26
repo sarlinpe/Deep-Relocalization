30d3aab97d7e6f305334d7bbb9336f83bc2ec86a
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2018-08-26 17:55:43 +0200

    Update Tango notebooks

--- notebooks/generate_proto_py.sh
+++ notebooks/generate_proto_py.sh
@@ -1,4 +1,11 @@
-MAPLAB="$MAPLAB/common/maplab-common/proto"
-PROTO="../deep-relocalization/proto/deep-relocalization"
+MAPLAB_COMMON="$MAPLAB/common/maplab-common/proto"
 
-protoc -I=$MAPLAB:$PROTO --python_out=./ $PROTO/descriptor_index.proto $MAPLAB/maplab-common/id.proto $MAPLAB/maplab-common/eigen.proto
+PROTO="../deep-relocalization/proto/deep-relocalization"
+protoc -I=$MAPLAB_COMMON:$PROTO --python_out=./ \
+    $PROTO/descriptor_index.proto \
+    $MAPLAB_COMMON/maplab-common/id.proto \
+    $MAPLAB_COMMON/maplab-common/eigen.proto
+
+
+PROTO="$MAPLAB/algorithms/loopclosure/loop-closure-handler/proto/loop-closure-handler"
+protoc -I=$MAPLAB_COMMON:$PROTO --python_out=./ $PROTO/debug_fusion.proto
--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
 
 def plot_imgs(imgs, titles=None, cmap='brg', ylabel='', normalize=True, ax=None,
@@ -33,3 +35,42 @@ def plot_imgs(imgs, titles=None, cmap='brg', ylabel='', normalize=True, ax=None,
             spine.set_visible(False)
     ax[0].set_ylabel(ylabel)
     plt.tight_layout()
+
+
+def draw_datches(img1, kp1, img2, kp2, matches, color=None, kp_radius=5,
+                 thickness=2, margin=20):
+    # Create frame
+    if len(img1.shape) == 3:
+        new_shape = (max(img1.shape[0], img2.shape[0]),
+                     img1.shape[1]+img2.shape[1]+margin,
+                     img1.shape[2])
+    elif len(img1.shape) == 2:
+        new_shape = (max(img1.shape[0],
+                     img2.shape[0]),
+                     img1.shape[1]+img2.shape[1]+margin)
+    new_img = np.ones(new_shape, type(img1.flat[0]))*255
+
+    # Place original images
+    new_img[0:img1.shape[0], 0:img1.shape[1]] = img1
+    new_img[0:img2.shape[0],
+            img1.shape[1]+margin:img1.shape[1]+img2.shape[1]+margin] = img2
+
+    # Draw lines between matches
+    if color:
+        c = color
+    for m in matches:
+        # Generate random color for RGB/BGR and grayscale images as needed.
+        if not color:
+            if len(img1.shape) == 3:
+                c = np.random.randint(0, 256, 3)
+            else:
+                c = np.random.randint(0, 256)
+            c = (int(c[0]), int(c[1]), int(c[2]))
+
+        end1 = tuple(np.round(kp1[m.trainIdx].pt).astype(int))
+        end2 = tuple(np.round(kp2[m.queryIdx].pt).astype(int)
+                     + np.array([img1.shape[1]+margin, 0]))
+        cv2.line(new_img, end1, end2, c, thickness, lineType=cv2.LINE_AA)
+        cv2.circle(new_img, end1, kp_radius, c, thickness, lineType=cv2.LINE_AA)
+        cv2.circle(new_img, end2, kp_radius, c, thickness, lineType=cv2.LINE_AA)
+    return new_img
