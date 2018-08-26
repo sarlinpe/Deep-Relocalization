433cb635aa42fa8ba24c0a0acabce58973f4c762
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2018-08-26 13:29:01 +0200

    Reorganize Google Landmarks downloading

--- utils/launch_cluster.sh
+++ utils/launch_cluster.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#output="-oo $(pwd)/out.txt"
-#output="-I"
 cores="15"
-memory="13500"  # per core
-scratch="5000"
-gpus="2"
-clock="120:00"
+memory="14000"  # per core
+scratch="1000"
+gpus="1"
+clock="4:00"
 model="GeForceGTX1080Ti"
 warn="-wt 15 -wa INT"
 
@@ -18,7 +16,3 @@ cmd="bsub
     $*"
 echo $cmd
 eval $cmd
-
-# best: bsub -n1-R " select[gpu_model0 == 'GeForceGTX1080’|| gpu_model1==       'GeForceGTX1080’||gpu_model2 == 'GeForceGTX1080’||gpu_model3 ==     'GeForceGTX1080’] rusage[ngpus_excl_p=1]”
-# see: https://github.com/prelz/BLAH/blob/master/src/scripts/lsf_submit.sh
-# check names with lsload -s | grep gpu_model
